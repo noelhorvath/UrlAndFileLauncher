be815900baf170eda140e9fd061101642468e1c1
noelhorvath <hnoel2000@gmail.com>
2022-02-10 22:32:32 +0100

    fix debug build script

--- build-debug.cmd
+++ build-debug.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Building debug...
 windres debug.rc debug_res.o
-g++ -Wall -o2 -municode -mwindows FileLauncher.cpp -o fl-debug.exe debug_res.o
+g++ -Wall -o2 -municode FileLauncher.cpp -o fl-debug.exe debug_res.o
 echo Debug version is ready.
