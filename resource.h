8ce505925cff17ea12434d46f10c7f0ff2364e54
noelhorvath <82838337+noelhorvath@users.noreply.github.com>
2022-02-12 21:44:56 +0100

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,10 +1,8 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by debug.rc
-//
+// Used by debug.rc and prod.rc
 
 // Next default values for new objects
-// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
