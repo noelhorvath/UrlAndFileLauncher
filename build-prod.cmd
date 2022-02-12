47f64f409bb2dfaa2a77140e83ddc5f9797ed776
noelhorvath <hnoel2000@gmail.com>
2022-02-12 22:06:21 +0100

    correct cleaned up file name

--- build-prod.cmd
+++ build-prod.cmd
@@ -3,6 +3,6 @@ echo Production build started...
 windres prod.rc prod_res.o
 g++ -Wall -O2 -municode -mwindows program.cpp uafl_api.cpp -o uafl.exe prod_res.o
 echo Cleaning up files...
-if exist debug_res.o del /f debug_res.o
+if exist prod_res.o del /f prod_res.o
 echo Production version is ready.
 exit /b 0
