77179a4cfeef3a27b8bd664ec9087f1a362efa1c
noelhorvath <hnoel2000@gmail.com>
2022-02-18 17:12:12 +0100

    update build scripts

--- build-debug.cmd
+++ build-debug.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo Debug build started...
 windres debug.rc debug_res.o
-g++ -Wall -municode program.cpp uafl_api.cpp -o uafl-debug.exe debug_res.o
+g++ -Wall -municode -std=c++23 program.cpp uafl_api.cpp -o uafl-debug.exe debug_res.o
 echo Cleaning up files...
 if exist debug_res.o del /f debug_res.o
 echo Debug version is ready.
--- build-prod.cmd
+++ build-prod.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo Production build started...
 windres prod.rc prod_res.o
-g++ -Wall -O2 -municode -mwindows program.cpp uafl_api.cpp -o uafl.exe prod_res.o
+g++ -Wall -O2 -municode -mwindows -std=c++23 program.cpp uafl_api.cpp -o uafl.exe prod_res.o
 echo Cleaning up files...
 if exist prod_res.o del /f prod_res.o
 echo Production version is ready.
